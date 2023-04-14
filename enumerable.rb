060cf63d2e1059d5e3fef255e43f9c5bf36e81c2
lenkon <inoulenkon@gmail.com>
2023-04-14 22:33:34 +0530

    Fix merge conflix

--- enumerable.rb
+++ enumerable.rb
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿module MyEnumerable
   def all?
     each { |obj| return false unless yield obj }
@@ -16,22 +15,3 @@
     arr
   end
 end
-=======
-module MyEnumerable
-    def all?
-      each { |obj| return false unless yield obj }
-      true
-    end
-  
-    def any?
-      each { |obj| return true if yield obj }
-      false
-    end
-  
-    def filter
-      arr = []
-      each { |obj| arr.push(obj) if yield obj }
-      arr
-    end
-  end
->>>>>>> ea6376ceb48f7129b79e8904822231b53e74cbc5
